28fc2f984c599975d4452b658632d3b57aebb770
MEGADROID-X23 <omegadroid4@gmail.com>
2025-02-01 09:22:00 -0800

    up5

--- payment.js
+++ payment.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 function makePayment(amount) {
         var handler = PaystackPop.setup({
             key:'pk_test_9a405a8f0a4c3466321f8b340bb805b7041e6792', // Replace with your PayStack public key
-            email: 'customer@example.com', // Replace with the customer's email address
+            email: 'gsmkeyunlocker@gmail.com', // Replace with the customer's email address
             amount: amount *100, // Replace with the amount to be paid in kobo
             currency: 'NGN', // Replace with the currency code (e.g. NGN for Naira)
             ref: '', // Replace with a unique reference for the order
